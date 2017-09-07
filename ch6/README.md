87f53d0d76c48157ab5f14e2be7a15196c3dbfc4
shun10114@gmail.com <shun10114@gmail.com>
2017-09-07 19:01:59 +0900

    [feature]Adding ch6, Vue Instance and Properties

--- ch6/README.md
+++ ch6/README.md
@@ -1,6 +1,8 @@
-# Conditionals & Lists
+# Vue Instance
 
-[Conditionals & Lists](https://vuejs.org/v2/guide/conditional.html)
+[Vue Instance - en](https://vuejs.org/v2/guide/instance.html)
+[Vue Instance - ko](https://kr.vuejs.org/v2/guide/instance.html)
 
-This chapter use all that we learn until ch5.
-- Simple project, Fighting Monster
+This chapter can learn about Vue Instance
+- Cross Vue Instance and many things you learn from here.
+- $refs , $data and so on, you can control javascript and html properties using VueJS
--- ch6/app.js
+++ ch6/app.js
@@ -1,26 +1,63 @@
-new Vue({
-  el: '#app',
-  data: {
+var data = {
     title: 'The VueJS Instance',
     showParagraph: false
-  },
-  methods: {
-    show: function() {
-      this.showParagraph = true;
-      this.updateTitle('The VueJS Instance (Updated)');
+};
+
+var vm1 = new Vue({
+    el: '#app1',
+    data: data,
+    methods: {
+        show: function () {
+            this.showParagraph = true;
+            this.updateTitle('The VueJS Instance (Updated)');
+            <!-- I can access all references I registered using the name -->
+            this.$refs.myButton.innerText = 'Refs Test';
+            console.log(this.$refs);
+        },
+        updateTitle: function (title) {
+            this.title = title;
+        }
     },
-    updateTitle: function(title) {
-      this.title = title;
+    computed: {
+        lowercaseTitle: function () {
+            return this.title.toLowerCase();
+        }
+    },
+    watch: {
+        title: function (value) {
+            alert('Title changed, new value: ' + value);
+        }
     }
-  },
-  computed: {
-    lowercaseTitle: function() {
-      return this.title.toLowerCase();
-    }
-  },
-  watch: {
-    title: function(value) {
-      alert('Title changed, new value: ' + value);
-    }
-  }
 });
+
+vm1.newProp = 'New!';
+//you can see this properties on the console.
+console.log(vm1);
+
+console.log(vm1.$data === data);
+
+vm1.$refs.heading.innerText = 'Something else';
+
+var vm2 = new Vue({
+    el: '#app2',
+    data: {
+        title: 'The VueJS Instance2',
+        showParagraph: false
+    },
+    methods: {
+        onChange: function () {
+            vm1.title = 'Changed by vm2 Button!!'
+        }
+    },
+    computed: {
+        lowercaseTitle: function () {
+            return this.title.toLowerCase();
+        }
+    },
+    watch: {
+        title: function (value) {
+            alert('Title changed, new value: ' + value);
+        }
+    }
+});
+
