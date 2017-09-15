8292ff75557b4b5a8b25ae63ed1033ec00c527a4
shun10114@gmail.com <shun10114@gmail.com>
2017-09-15 12:03:13 +0900

    [feature:dynamo]Adding dynamo files

---
+++ practice/axios/ch1/dynamo.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data: {
+    	question: 'Test1',
+    	question2: 'Test2'
+    },
+    methods: {},
+    computed: {},
+    watch: {}
+});
+
---
+++ practice/axios/dynamo.html
@@ -0,0 +1,1 @@
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
